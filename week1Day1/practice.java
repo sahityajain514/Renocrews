2c0bbfa1e188e4320088bbe9f2186250acd36c45
sahityajain514 <133737716+sahityajain514@users.noreply.github.com>
2024-01-16 22:33:59 +0530

    Update practice.java

--- week1Day1/practice.java
+++ week1Day1/practice.java
@@ -1,1 +1,56 @@
+import java.util.*; 
 
+class Main {
+  public static void main(String[] args) {
+
+    //Question number 1
+// Scanner sc=new Scanner(System.in);
+//     System.out.println("enter three numbers");
+//     int a=sc.nextInt();
+//     int b=sc.nextInt();
+//     int c=sc.nextInt();
+//     int sum=a+b+c;
+//     System.out.println("sum of three numbers is "+sum);
+
+
+
+
+    //Question number 2
+    // Scanner sc=new Scanner(System.in);
+    //     System.out.println("enter three subject marks");
+    //     int a=sc.nextInt();
+    //     int b=sc.nextInt();
+    //     int c=sc.nextInt();
+    //     int sum=a+b+c;
+    // float cgpa=(sum/30);
+    //     System.out.println("cgpa of three numbers is "+cgpa);
+
+
+
+    //Question 3
+    // Scanner sc=new Scanner(System.in);
+    // System.out.println("enter yr name");
+    // String str=sc.nextLine();
+    // System.out.println("Hello "+str+" have a good day!");
+
+
+    //Question 4
+    // double kilometers;
+
+    // System.out.println("Please enter kilometers:");
+
+    // Scanner sc = new Scanner(System.in);
+    // kilometers = sc.nextDouble();
+
+    // double miles = kilometers / 1.6;
+
+    // System.out.println(miles + " Miles");
+
+
+    //Question 5
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter SOMETHING  that you want to check it is an integer or not");
+    
+    System.out.println(sc.hasNextInt());
+  }
+}
