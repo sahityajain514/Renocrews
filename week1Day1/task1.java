b68782037a49ffa858da894a5d5877ec685d1059
sahityajain514 <133737716+sahityajain514@users.noreply.github.com>
2024-01-16 22:42:10 +0530

    Update task1.java

--- week1Day1/task1.java
+++ week1Day1/task1.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the 5 subject marks out of 100");
+    int sub1=sc.nextInt();
+    int sub2=sc.nextInt();
+    int sub3=sc.nextInt();
+    int sub4=sc.nextInt();
+    int sub5=sc.nextInt();
+    int total=sub1+sub2+sub3+sub4+sub5;
+    float per=(total*100)/500;
+    System.out.println("percentage of a student is "+per+"%");
+  }
+
+  
+}
