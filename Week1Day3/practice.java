eecd70359927a409a0b9f4948e2afb058a22ee95
sahityajain514 <133737716+sahityajain514@users.noreply.github.com>
2024-01-17 19:33:08 +0530

    Create practice.java

---
+++ Week1Day3/practice.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+//Question 1
+    // String name="SAHITYA JAIN";
+    // name=name.toLowerCase();
+    // System.out.println(name);
+
+    //Question 2
+    // String str="hello dear friend";
+    // str=str.replace(" ","_");
+    // System.out.println(str);
+
+    //Question 3
+    // String str="Dear |<name>| ,thanks a lot";
+    // str=str.replace("|<name>|","Sahitya");
+    // System.out.println(str);
+
+    //Question 4
+    // String str="this string contains double  and tripple     spaces";
+    // System.out.println(str.indexOf("  "));
+    // System.out.println(str.indexOf("   "));
+
+
+    //Question 5
+    String str="Dear jain sahab, \n\tThis Java Course is nice.\n\t you are learning it by your pace";
+    System.out.println(str);
+  }
+
+  
+}
