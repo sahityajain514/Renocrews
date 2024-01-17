c839b30360b3b7681513e301ec9bf3e683df9bb2
sahityajain514 <133737716+sahityajain514@users.noreply.github.com>
2024-01-17 18:38:29 +0530

    Update practice.java

--- Week1Day2/practice.java
+++ Week1Day2/practice.java
@@ -1,1 +1,43 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
 
+
+    //problem 1
+    // float a=7/4.0f*9/2.0f;
+    // System.out.println(a);
+
+
+    //problem 2
+    // char grade='B';
+    // grade=(char)(grade+8);
+    // System.out.println(grade);
+    // //after reduction
+    // grade=(char)(grade-8);
+    // System.out.println(grade);
+
+    //problem 3
+    // Scanner sc=new Scanner(System.in);
+    // System.out.println("enter any number");
+    // int a=sc.nextInt();
+    // System.out.println(a>10);
+
+
+    //problem 4
+    // float v=7f;
+    // float u=4f;
+    // float a=6f;
+    // float s=32f;
+    // float ans=(v*v)-(u*u)/(2*a*s);
+    // System.out.println(ans);
+    
+    //problem 5
+    int x=7;
+    int a=7*49/7+35/7;
+    System.out.println(a);
+    
+  }
+
+  
+  
+}
