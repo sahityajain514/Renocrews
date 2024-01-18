4a3eac45114f52d921670b687f267cf8218666cc
sahityajain514 <133737716+sahityajain514@users.noreply.github.com>
2024-01-18 19:21:15 +0530

    Create practiceSet.java

---
+++ Week1Day4/practiceSet.java
@@ -0,0 +1,113 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+  //Question 1
+    // int a=10;
+    // if(a=11){
+    //   System.out.println("I am 11");
+      
+    // }
+    // else{
+    //   System.out.println("I am not 11");
+    // }
+
+
+  //Output will be
+    //incompatible types: int cannot be converted to boolean
+
+
+    //Question 2
+    // int marks1,marks2,marks3;
+    // Scanner sc=new Scanner(System.in);
+    // System.out.println("enter mathematics marks : ");
+    // marks1=sc.nextInt();
+    // System.out.println("enter physics marks : ");
+    // marks2=sc.nextInt();
+    // System.out.println("enter chemistry marks : ");
+    // marks3=sc.nextInt();
+    // float avg=(marks1+marks2+marks3)/3.0f;
+    // System.out.println("your percentage is : "+avg);
+    // if(avg>=40 && marks1>=33 && marks2>=33 && marks3>=33){
+    //   System.out.println("Congratulations, you have been promoted");
+    // }
+    // else{
+    //   System.out.println("Sorry, you have not been promoted");
+    // }
+
+
+    //Question 3
+       // Scanner sc = new Scanner(System.in);
+       //       System.out.println("Enter your income in Lakhs per annum");
+       //       float tax = 0;
+       //       float income = sc.nextFloat();
+       //       if(income<=2.5){
+       //           tax = tax + 0;
+       //       }
+       //       else if(income>2.5f && income <= 5f){
+       //           tax = tax + 0.05f * (income - 2.5f);
+       //       }
+       //       else if(income>5f && income <= 10.0f){
+       //           tax = tax + 0.05f * (5.0f - 2.5f);
+       //           tax = tax + 0.2f * (income - 5f);
+       //       }
+       //       else if(income>10.0f){
+       //           tax = tax + 0.05f * (5.0f - 2.5f);
+       //           tax = tax + 0.2f * (10.0f - 5f);
+       //           tax = tax + 0.3f * (income - 10.0f);
+       //       }
+      
+       //       System.out.println("The total tax paid by the employee is:"+tax);
+
+
+
+
+    //Question 4
+    //  Scanner sc = new Scanner(System.in);
+    // System.out.println("enter the day");
+    //        int day = sc.nextInt();
+    
+    //        switch (day){
+    //            case 1 -> System.out.println("Monday");
+    //            case 2 -> System.out.println("Tuesday");
+    //            case 3 -> System.out.println("Wednesday");
+    //            case 4 -> System.out.println("Thursday");
+    //            case 5 -> System.out.println("Friday");
+    //            case 6 -> System.out.println("Saturday");
+    //            case 7 -> System.out.println("Sunday");
+    //            default -> System.out.println("Invalid day");
+    //        }
+
+//Question 5
+    // Scanner sc=new  Scanner(System.in);
+    // System.out.println("enter the value of year");
+    // int year = sc.nextInt();
+
+    //  if (year % 400 == 0)
+    //    System.out.println (year + " is a Leap Year");
+
+    //  else if (year % 4 == 0 && year % 100 != 0)
+    //    System.out.println (year + " is a Leap Year");
+
+    //  else
+    //      System.out.println (year + " is not a Leap Year");
+
+
+    
+    //Question 6
+    //   Scanner sc = new Scanner(System.in);
+    // System.out.println("enter any website name with domain name");
+    //        String website = sc.next();
+    //        if(website.endsWith(".org")){
+    //            System.out.println("This is an organizational website");
+    //        }
+    //        else if(website.endsWith(".com")){
+    //            System.out.println("This is a Commercial website");
+    //        }
+    //        else if(website.endsWith(".in")){
+    //            System.out.println("This is an Indian website");
+    //        }
+  }
+
+  
+}
