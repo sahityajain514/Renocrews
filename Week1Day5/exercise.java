13ca87c1efa931e4c65d41e187fbb1448a44700f
sahityajain514 <133737716+sahityajain514@users.noreply.github.com>
2024-01-19 23:29:14 +0530

    Create exercise.java

---
+++ Week1Day5/exercise.java
@@ -0,0 +1,102 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+    //Question 1
+    // for(int i=4;i>0;i--){
+    //   for(int j=0;j<i;j++){
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+    // }
+
+
+    //Question 2
+    // System.out.println("Enter the number ");
+    // int n=sc.nextInt();
+    // int sum=0;
+    // int i=0;
+    // while(n>0){
+    //   sum=sum+2*i;
+    //   i++;
+    //   n--;
+    // }
+    // System.out.println(sum);
+
+
+    //Question 3
+    // System.out.println("Enter the number ");
+    // int n=sc.nextInt();
+    // for(int i=1;i<=10;i++){
+    //   System.out.println(n+"X"+i+"="+n*i);
+    // }
+
+    //Question 4
+    // for(int i=10;i>=1;i--){
+    //   System.out.println(10+"X"+i+"="+10*i);
+    // }
+
+
+    //Question 5
+    // System.out.println("Enter the number ");
+    // int n=sc.nextInt();
+    // int fact=1;
+    // for(int i=1;i<=n;i++)
+    //   {
+    //     fact=fact*i;
+    //   }
+    // System.out.println(fact);
+
+
+    //Question 6
+    // System.out.println("Enter the number ");
+    // int n=sc.nextInt();
+    // int fact=1;
+    // while(n>=1){
+    //   fact=fact*n;
+    //   n--;
+    // }
+    //  System.out.println(fact);
+
+
+    //Question 7
+    // for(int i=4;i>0;i--){
+    //     for(int j=0;j<i;j++){
+    //       System.out.print("*");
+    //     }
+    //     System.out.println();
+    //   }
+
+    //Question 8
+    //true
+
+
+    //Question 9
+    // int sum=0;
+    // System.out.println("Enter the number ");
+    // int n=sc.nextInt();
+    // for(int i=1;i<=10;i++){
+    //   sum=sum+n*i;
+    // }
+    // System.out.println(sum);
+
+    //Question 10
+    // at most one time
+
+
+    //Question 11
+    
+    System.out.println("Enter the number ");
+    int n=sc.nextInt();
+    int sum=0;
+    
+    for(int i=0;i<n;i++){
+      sum=sum+2*i;
+     
+    }
+    System.out.println(sum);
+  }
+
+ 
+}
