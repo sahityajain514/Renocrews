cc578a55abac5849b586f04a6272d658460b855b
sahityajain514 <133737716+sahityajain514@users.noreply.github.com>
2024-02-25 00:52:58 +0530

    Update InsertTable.java

--- Week5/Day3/InsertTable.java
+++ Week5/Day3/InsertTable.java
@@ -1,17 +1,31 @@
+import java.io.*;
 import java.sql.*;
 public class InsertTable {
     public static void main(String[] args) {
 try{
     Class.forName("com.mysql.cj.jdbc.Driver");
-    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabasetoday","root","Pass");
+    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabasetoday","root","Papa@199901SJ");
     //create query
     String query="insert into table1(tName,tCity) values(?,?)";
     //PreparedStatement object
     PreparedStatement ps=con.prepareStatement(query);
     //set the values to the query
-    ps.setString(1,"Sahitya jain");
-    ps.setString(2,"Damoh");
+//    ps.setString(1,"Sanyam jain");
+//    ps.setString(2,"Dabra");
+//    ps.executeUpdate();
+
+    //dynamic changes
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter Name");
+    String name=br.readLine();
+    System.out.println("Enter City");
+    String city=br.readLine();
+    ps.setString(1,name);
+    ps.setString(2,city);
     ps.executeUpdate();
+
+
+
     System.out.println("inserted");
     ps.close();
     con.close();
